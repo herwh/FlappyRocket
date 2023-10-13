d4503b2010cfeedf649300bc1072489556c96e16
herwh <herwh@yandex.ru>
2023-10-13 13:45:12 +0300

    Added obstacle oscillator

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] private Vector3 _movementVector;
+    [SerializeField, Range (0,1)] private float _movementFactor;
+    [SerializeField] private float _period=0.5f;
+    
+    private Vector3 _startingPosition;
+    
+    void Start()
+    {
+        _startingPosition = transform.position;
+    }
+
+    void Update()
+    {
+        var cycle = Time.time / _period;
+        var sinWave = Mathf.Sin(cycle);
+        _movementFactor = (sinWave + 1f )/ 2f;
+        
+        var offset = _movementVector * _movementFactor;
+        transform.position = _startingPosition + offset;
+    }
+}
